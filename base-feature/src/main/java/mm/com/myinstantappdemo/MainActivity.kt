69a538b72e5d0919d6681e1cd8fc0a5b847c674f
milan <milan@example.com>
2021-04-14 13:28:55 +0200

    -initial commit -added instant feature

--- base-feature/src/main/java/mm/com/myinstantappdemo/MainActivity.kt
+++ base-feature/src/main/java/mm/com/myinstantappdemo/MainActivity.kt
@@ -1,12 +1,20 @@
 package mm.com.myinstantappdemo
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.btnMain
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        btnMain.setOnClickListener {
+            val i = Intent(this,DetailActivity::class.java)
+            startActivity(i)
+        }
     }
 }
