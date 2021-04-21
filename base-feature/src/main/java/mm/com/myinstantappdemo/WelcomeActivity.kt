4c6ee8065cd1fc6b430c89d7b96dd7444699ae72
milan <milan@example.com>
2021-04-21 14:39:18 +0200

    -modified WelcomeActivity layout -added InitialActivity.kt -added drawable res -added Pending animations in res -added animation resources -added drawable resources -added exit dialog -added device checkers in DetailActivity.kt

--- base-feature/src/main/java/mm/com/myinstantappdemo/WelcomeActivity.kt
+++ base-feature/src/main/java/mm/com/myinstantappdemo/WelcomeActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.rlBtnNext
+import mm.com.myinstantappdemo.tools.showExitAlertDialog
 
 class WelcomeActivity : AppCompatActivity() {
 
@@ -15,6 +16,12 @@ class WelcomeActivity : AppCompatActivity() {
         rlBtnNext.setOnClickListener {
             val i = Intent(this, DetailActivity::class.java)
             startActivity(i)
+            overridePendingTransition(R.anim.enter_from_right, R.anim.exit_to_left)
         }
+    }
+
+    override fun onBackPressed() {
+        super.onBackPressed()
+        this.showExitAlertDialog { finishAffinity() }
     }
 }
